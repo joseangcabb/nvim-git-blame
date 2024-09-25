b860ed8e418785bcb84af8bf330d1e02fe1c6b8e
Jose Angel <angcabb@gmail.com>
2024-09-24 20:25:53 -0500

    Use on_stdout event to return data

--- lua/git_blame/git.lua
+++ lua/git_blame/git.lua
@@ -1,21 +1,11 @@
 local Git = {}
 
 local function exec_cmd(cmd, callback)
-  local output = {}
   vim.fn.jobstart(cmd, {
     stdout_buffered = true,
     on_stdout = function(_, data, _)
-      if data and type(data) == "table" then
-        for _, line in ipairs(data) do
-          if line and #line > 0 then
-            table.insert(output, line)
-          end
-        end
-      end
-    end,
-    on_exit = function(_, exit_code, _)
-      if exit_code == 0 and callback then
-        callback(output)
+      if data then
+        callback(data)
       end
     end,
   })
