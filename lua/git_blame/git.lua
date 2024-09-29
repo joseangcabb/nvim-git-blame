6b709c2db01c0703d176f12d444b20a0abca203f
Jose Angel <angcabb@gmail.com>
2024-09-28 19:33:21 -0500

    Improve method to fetch metadata

--- lua/git_blame/git.lua
+++ lua/git_blame/git.lua
@@ -1,15 +1,5 @@
+local Utils = require("git_blame.utils")
 local Git = {}
-
-local function exec_cmd(cmd, callback)
-  vim.fn.jobstart(cmd, {
-    stdout_buffered = true,
-    on_stdout = function(_, data, _)
-      if data then
-        callback(data)
-      end
-    end,
-  })
-end
 
 function Git:new(object)
   object = object or {}
@@ -20,15 +10,47 @@ end
 
 function Git:blame(file, callback)
   local result = {}
-  local cmd = {"git", "blame", file}
+  local cmd = { "git", "blame", "--line-porcelain", "-L", "1,3", file }
 
-  exec_cmd(cmd, function(output)
-    for _, line in ipairs(output) do
-      local commit = line:match("^(%x+)")
-      table.insert(result, commit)
+  Utils.exec_cmd(cmd, function(output)
+    local code_line = nil
+
+    for _, raw_data in ipairs(output) do
+      if not code_line then
+        local hash, temp_code_line = string.match(raw_data, "^(%w+)%s%d+%s(%d+)")
+
+        if hash and temp_code_line then
+          code_line = temp_code_line
+          result[code_line] = result[code_line] or {}
+          result[code_line].hash = string.sub(hash, 1, 6)
+        end
+      else
+        local author = string.match(raw_data, "^author%s(.*)$")
+        if author then
+          result[code_line].author = author
+        end
+
+        local time = string.match(raw_data, "^committer%-time%s(.*)$")
+        if time then
+          result[code_line].time = os.date("%B %d, %Y at %I:%M %p", time)
+        end
+
+        local message = string.match(raw_data, "^summary%s(.*)$")
+        if message then
+          result[code_line].message = message
+        end
+
+        if result[code_line].hash and
+            result[code_line].author and
+            result[code_line].time and
+            result[code_line].message then
+          code_line = nil
+        end
+      end
     end
 
     if callback then
+      P(result)
       callback(result)
     end
   end)
