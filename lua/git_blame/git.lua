7a4698ded94443d944563d566477ca909686fd68
Jose Angel <angcabb@gmail.com>
2024-10-06 23:51:15 -0500

    Refactor block parsing algorithm

--- lua/git_blame/git.lua
+++ lua/git_blame/git.lua
@@ -8,43 +8,43 @@ function Git:new(object)
   return object
 end
 
+function parseMetadata(raw_line)
+  local author = string.match(raw_line, "^author%s(.*)$")
+  if author then
+    return "author", author
+  end
+
+  local time = string.match(raw_line, "^committer%-time%s(.*)$")
+  if time then
+    return "time", os.date("%B %d, %Y at %I:%M %p", time)
+  end
+
+  local message = string.match(raw_line, "^summary%s(.*)$")
+  if message then
+    return "message", message
+  end
+
+  return nil, nil
+end
+
 function Git:blame(file, callback)
   local result = {}
-  local cmd = { "git", "blame", "--line-porcelain", "-L", "1,3", file }
+  local cmd = { "git", "blame", "--line-porcelain", "-L", "3,10", file }
 
   Utils.exec_cmd(cmd, function(output)
-    local code_line = nil
+    local current_line = nil
 
-    for _, raw_data in ipairs(output) do
-      if not code_line then
-        local hash, temp_code_line = string.match(raw_data, "^(%w+)%s%d+%s(%d+)")
-
-        if hash and temp_code_line then
-          code_line = temp_code_line
-          result[code_line] = result[code_line] or {}
-          result[code_line].hash = string.sub(hash, 1, 6)
-        end
+    for _, raw_line in ipairs(output) do
+      local hash, line_number = string.match(raw_line, "^(%w+)%s%d+%s(%d+)")
+      if hash and line_number then
+        current_line = tonumber(line_number)
+        result[current_line] = {
+          hash = string.sub(hash, 1, 6)
+        }
       else
-        local author = string.match(raw_data, "^author%s(.*)$")
-        if author then
-          result[code_line].author = author
-        end
-
-        local time = string.match(raw_data, "^committer%-time%s(.*)$")
-        if time then
-          result[code_line].time = os.date("%B %d, %Y at %I:%M %p", time)
-        end
-
-        local message = string.match(raw_data, "^summary%s(.*)$")
-        if message then
-          result[code_line].message = message
-        end
-
-        if result[code_line].hash and
-            result[code_line].author and
-            result[code_line].time and
-            result[code_line].message then
-          code_line = nil
+        local key, value = parseMetadata(raw_line)
+        if key and value then
+          result[current_line][key] = value
         end
       end
     end
