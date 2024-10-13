4fc98257c28455aa4c27ff6cfc41491c4152c1de
Jose Angel <angcabb@gmail.com>
2024-10-13 03:15:44 -0500

    Add typing and refactor git stuff

--- lua/git_blame/git.lua
+++ lua/git_blame/git.lua
@@ -1,50 +1,63 @@
-local Utils = require("git_blame.utils")
-local Git = {}
+local Utils = require "git_blame.utils"
 
+-- Module to handle blame logic in git
+local Git = {}
+Git.__index = Git
+
+-- Create a new Git instance
+-- @param object table: An optional table to initialize the instance
+-- @return Git: A new instance of Git
 function Git:new(object)
-  object = object or {}
-  setmetatable(object, self)
-  self.__index = self
-  return object
+  return setmetatable(object or {}, self)
 end
 
-function parseMetadata(raw_line)
-  local author = string.match(raw_line, "^author%s(.*)$")
-  if author then
-    return "author", author
-  end
+-- Table with patterns to extract metadata
+local metadata_patterns = {
+  hash_and_line_number = "^(%w+)%s%d+%s(%d+)",
+  author = "^author%s(.*)$",
+  time = "^committer%-time%s(.*)$",
+  message = "^summary%s(.*)$",
+}
 
-  local time = string.match(raw_line, "^committer%-time%s(.*)$")
-  if time then
-    return "time", os.date("%B %d, %Y at %I:%M %p", time)
-  end
-
-  local message = string.match(raw_line, "^summary%s(.*)$")
-  if message then
-    return "message", message
+-- Function to parse the metadata of a blame line
+-- @param raw_line string: A line from git blame output
+-- @return string|nil, string|nil: Returns key-value pairs for metadata or nil
+local function parseMetadata(raw_line)
+  for key, pattern in pairs(metadata_patterns) do
+    local value = string.match(raw_line, pattern)
+    if value then
+      return key, value
+    end
   end
 
   return nil, nil
 end
 
+-- Main function to execute the blame command and process the result
+-- @param file string: The file path on which to execute the git blame command
+-- param callback function: A function to call with the result
 function Git:blame(file, callback)
   local result = {}
   local cmd = { "git", "blame", "--line-porcelain", "-L", "3,10", file }
 
   Utils.exec_cmd(cmd, function(output)
-    local current_line = nil
+    local code_line = nil
 
     for _, raw_line in ipairs(output) do
-      local hash, line_number = string.match(raw_line, "^(%w+)%s%d+%s(%d+)")
+      -- If the output line begins with a hash followed by a line number,
+      -- extract the line number and use it to record the associated metadata.
+      local hash, line_number = string.match(raw_line, metadata_patterns.hash_and_line_number)
       if hash and line_number then
-        current_line = tonumber(line_number)
-        result[current_line] = {
-          hash = string.sub(hash, 1, 6)
-        }
-      else
+        code_line = tonumber(line_number)
+        if code_line then
+          result[code_line] = {
+            hash = string.sub(hash, 1, 6)
+          }
+        end
+      elseif code_line then -- Record metadata for the specific code line
         local key, value = parseMetadata(raw_line)
         if key and value then
-          result[current_line][key] = value
+          result[code_line][key] = value
         end
       end
     end
